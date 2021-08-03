3e15a4b64ff9ca84c4ab38bb80fd6a106b1ad1bf
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-03 19:07:56 +0500

    Display the message

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let btn = document.getElementById("btn")
+
+
 let firstCard = 10
 let secondCard = 4
 let sum = firstCard + secondCard
@@ -5,14 +8,21 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
-console.log(message)
+
