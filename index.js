838c6a2b6ca34b11f5c39b9095cdfe2117426045
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-03 19:14:11 +0500

    Display the sum

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ let btn = document.getElementById("btn")
 
 
 let firstCard = 10
-let secondCard = 4
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
 
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
+    sumEl.textContent = "Sum:" + sum
 }
 
 
