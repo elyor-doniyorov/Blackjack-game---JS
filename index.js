24dd4d68fa0b691a14965daa4fa07df5802748db
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-04 10:49:37 +0500

    Add new card function

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 
 function startGame() {
-    sumEl.textContent = "Sum:" + sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum:" + sum;
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
-        hasBlackJack = true
+        message = "Whoa! You've got Blackjack!"
+        hasBlackJack = true;
     } else {
         message = "You're out of the game!"
-        isAlive = false
+        isAlive = false;
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
 function newCard() {
     console.log("Drawing a new card from the deck!")
+    let thirdCard = 7;
+    sum += thirdCard;
+    startGame()
 }
 
 
